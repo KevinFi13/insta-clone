50090ec5733daef0a058d311a737f61cff146c35
Kevin Ferrandiz Izquierdo <kevinfi1396@gmail.com>
2019-06-18 22:45:01 -0400

    added xCode project

---
+++ insta-clone/insta-clone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  insta-clone
+//
+//  Created by Kevin Ferrandiz Izquierdo on 2019-06-18.
+//  Copyright © 2019 Kevin Ferrandiz Izquierdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello World!");
+    }
+
+
+}
+
